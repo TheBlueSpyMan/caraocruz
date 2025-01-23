37655bf520f778968ca31f1633213589b9620bce
DESKTOP-65SAUST\Alumno <victor2003aguilar@gmail.com>
2025-01-23 15:24:03 +0100

    Coin flipping fix with time delay

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    console.log("Online jQuery!");
-})
+    let $coin = $("#coin");
+
+    $coin.on("click", function(){
+        let randomNum = Math.random();
+        $coin.removeClass();
+
+        setTimeout(function(){
+
+            if (randomNum <= 0.5) {
+                console.log("cara")
+                $coin.addClass("isHeads");
+            } else {
+                console.log("cruz")
+                $coin.addClass("isTails");
+            }
+
+        }, 100);
+   
+
+    });
+
+
+});
